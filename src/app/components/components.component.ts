60abef80222218538d9fb8cd94cec60129edb12b
MyvinB <myvinbarboza@gmail.com>
2020-06-25 18:04:40 +0530

    latest

---
+++ src/app/components/components.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { NgbDateStruct, NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+    selector: 'app-components',
+    templateUrl: './components.component.html',
+    styles: [`
+    ngb-progressbar {
+        margin-top: 5rem;
+    }
+
+    `]
+})
+
+export class ComponentsComponent implements OnInit {
+
+      images = [{img:"assets/img/1.jpeg",title:"Herbal Cosmetic products pack by Biotique",cost:799},{img:"assets/img/2.jpeg",title:"Phone Casings by Q-Tech",cost:399},{img:"assets/img/3.jpeg",title:"Organic Tea by Chopra Center",cost:399},{img:"assets/img/4.jpeg",title:"Best seller combo by Nayantara Sahgal",cost:499}];
+        
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+
+  @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (this.unpauseOnArrow && slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
+  }
+
+
+    page = 4;
+    page1 = 5;
+    focus;
+    focus1;
+    focus2;
+    date: {year: number, month: number};
+    model: NgbDateStruct;
+    constructor( private renderer : Renderer2) {}
+    isWeekend(date: NgbDateStruct) {
+        const d = new Date(date.year, date.month - 1, date.day);
+        return d.getDay() === 0 || d.getDay() === 6;
+    }
+
+    isDisabled(date: NgbDateStruct, current: {month: number}) {
+        return date.month !== current.month;
+    }
+
+    ngOnInit() {
+        let input_group_focus = document.getElementsByClassName('form-control');
+        let input_group = document.getElementsByClassName('input-group');
+        for (let i = 0; i < input_group.length; i++) {
+            input_group[i].children[0].addEventListener('focus', function (){
+                input_group[i].classList.add('input-group-focus');
+            });
+            input_group[i].children[0].addEventListener('blur', function (){
+                input_group[i].classList.remove('input-group-focus');
+            });
+        }
+    }
+
+}
